3ec98209d5a93a3d3b2a26b841bae69f69e9dec2
Nicholas Tovik <ntovik@stringray.local>
2016-06-09 10:33:39 -0700

    some checks and timing

--- ticklemypickle.py
+++ ticklemypickle.py
@@ -10,14 +10,21 @@ import json
 import time
 from textblob import TextBlob
 
-handleTime = datetime.timedelta(minutes=1).seconds
+friendshiptime = datetime.timedelta(minutes=1).seconds
+handleTime = datetime.timedelta(minutes=30).seconds
 TIMEOUT = datetime.timedelta(minutes=60).seconds
+
 
 def auth():
     with open("access.json", 'r') as f:
         db = json.load(f)
-    twitter = Twython(db["API_Key"], db["API_Secret"], db["Access_Token"], db["Access_Token_Secret"])
+    twitter = Twython(
+        db["API_Key"],
+        db["API_Secret"],
+        db["Access_Token"],
+        db["Access_Token_Secret"])
     return twitter
+
 
 def main():
     output = buildPost()
@@ -29,16 +36,25 @@ def main():
 #    countdown()
     retweet()
 
+
 def retweet():
-    words_to_rt = ["#butter", "#flavortown", "#GroceryGames", "#guyfieri", "#foodnetwork", "#porkfat", "cheflife"]
+    words_to_rt = [
+        "#butter",
+        "#flavortown",
+        "#GroceryGames",
+        "#guyfieri",
+        "#foodnetwork",
+        "#porkfat",
+        "cheflife"]
     hashlist = " OR ".join(words_to_rt)
     twitter = auth()
     search_results = twitter.search(q=hashlist, count=5)
     try:
         for tweet in search_results["statuses"]:
-            twitter.retweet(id = tweet["id_str"])
+            twitter.retweet(id=tweet["id_str"])
     except TwythonError as e:
         print e
+
 
 def buildTweet():
     chain = pickle.load(open("chain.p", "rb"))
@@ -53,6 +69,7 @@ def buildTweet():
     tweet = ' '.join(new_tweet)
     return tweet
 
+
 def buildPost():
     output = ''
     while len(output) < 120:
@@ -62,7 +79,23 @@ def buildPost():
 
 
 def hashtag(output):
-    keywords = ["outofbounds", "DDD", "420braiseit", "flavortown", "badabing", "brother", "wow", "bbq", "rollingout", "guyfieri", "tripleD", "gangsta", "cheflife", "outofthisworld", "saucy", "shutthefrontdoor"]
+    keywords = [
+        "outofbounds",
+        "DDD",
+        "420braiseit",
+        "flavortown",
+        "badabing",
+        "brother",
+        "wow",
+        "bbq",
+        "rollingout",
+        "guyfieri",
+        "tripleD",
+        "gangsta",
+        "cheflife",
+        "outofthisworld",
+        "saucy",
+        "shutthefrontdoor"]
     hashtag = str(random.choice(keywords))
     print "#" + hashtag
     return hashtag
@@ -82,12 +115,14 @@ def friendshipiscreepy():
     last_sent_id = []
     lastsent = twitter.get_direct_messages(since_id=last_sent_id)
     friendId = lastsent[0]['id_str']
+    who = lastsent[0]['sender_screen_name']
+    dtext = lastsent[0]['text']
     if friendId in open('twitterdm.log').read():
         pass
 
     else:
         lf = open('twitterdm.log', 'a+')
-        lf.write(str(friendId) + '\n')
+        lf.write(str(friendId) + " " + who + " " + dtext + '\n')
         print friendId
         targets = twitter.get_direct_messages(count=1)
         for target in targets:
@@ -102,36 +137,42 @@ def friendshipiscreepy():
                     print(e)
         lf.close()
 
+
 def handlementions(lastMentionId=None):
     twitter = auth()
     mentions = twitter.get_mentions_timeline(since_id=lastMentionId)
     if mentions:
-        # Remember the most recent tweet id, which will be the one at index zero.
+        # Remember the most recent tweet id, which will be the one at index
+        # zero.
         lastMentionId = mentions[0]['id_str']
         for mention in mentions:
             who = mention['user']['screen_name']
             text = mention['text']
             theId = mention['id_str']
-
+            rlog = open('twitterreply.log', 'a+')
             # we favorite every mention that we see
             try:
-                twitter.create_favorite(id=theId)
-                # create a reply to them.
-                msg = buildTweet()
-                # In order to post a reply, you need to be sure to include
-                # their username in the body of the tweet.
-                replyMsg = "@{0} {1}".format(who, msg)
-                print replyMsg
-                twitter.update_status(status=replyMsg, in_reply_to_status_id=theId)
+                if lastMentionId not in open('twitterreply.log').read():
+                    rlog.write(str(theId) + " " + who + " " + text + '\n')
+                    twitter.create_favorite(id=theId)
+                    # create a reply to them.
+                    msg = buildTweet()
+                    # In order to post a reply, you need to be sure to include
+                    # their username in the body of the tweet.
+                    replyMsg = "@{0} {1}".format(who, msg)
+                    print replyMsg
+                    twitter.update_status(
+                        status=replyMsg, in_reply_to_status_id=theId)
+                else:
+                    print "No new mentions"
             except TwythonError as e:
                 print e
 
 if __name__ == '__main__':
     h = task.LoopingCall(handlementions)
     f = task.LoopingCall(friendshipiscreepy)
-    f.start(handleTime)
+    f.start(friendshiptime)
     h.start(handleTime)
     l = task.LoopingCall(main)
     l.start(TIMEOUT)
     reactor.run()
-

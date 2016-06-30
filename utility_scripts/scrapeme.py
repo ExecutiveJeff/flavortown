73c8774be491dc08a3e2bf5ef400318d27f64a43
ntovik <nick.tovik@callsocket.com>
2016-06-30 10:07:14 -0700

    restructuring

---
+++ utility_scripts/scrapeme.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python -tt
+
+import urllib2
+from lxml.html import fromstring
+import sys
+import time
+
+urlprefix = "http://stuffhipsterssay.tumblr.com/"
+
+# 709
+for page in xrange(1, 10):
+    try:
+        out = "-> On page {} of {}....      {}%"
+        print out.format(page, "10", str(round(float(page) / 10 * 100, 2)))
+        response = urllib2.urlopen(urlprefix + str(page))
+        html = response.read()
+        dom = fromstring(html)
+        sels = dom.xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "short", " " ))]')
+        for review in sels:
+            if review.text:
+                print review.text.rstrip()
+        sys.stdout.flush()
+        time.sleep(2)
+    except:
+        continue

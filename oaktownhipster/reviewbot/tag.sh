383bd70bc3905d8b4e6f2f08e0536ebc4f0aa3cb
ntovik <nick.tovik@callsocket.com>
2016-06-30 10:08:00 -0700

    cleaning

--- oaktownhipster/reviewbot/tag.sh
+++
@@ -1,4 +0,0 @@
-grep -E -v '^-' $1 > prep.txt
-cat prep.txt | sed 's/^/BEGIN NOW /' > prep2.txt
-cat prep2.txt | sed 's/$/ END/' > finish.txt
-rm prep.txt && rm prep2.txt
--- oaktownhipster/reviewcleaner.sh
+++
@@ -1,4 +0,0 @@
-tr  '.' '\n' < $1 > dummy.txt
-sed '/^$/d' dummy.txt > dumb.txt
-sed 's/$/./' dumb.txt > clean.txt
-rm dummy.txt && rm dumb.txt
